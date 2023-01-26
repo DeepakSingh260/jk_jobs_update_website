701ab3862a303c8a5c19ed873eff3d74122c9429
ayush391 <stringakaak@gmail.com>
2023-01-26 11:16:47 +0530

    added post page

--- src/assets/pages/post.jsx
+++ src/assets/pages/post.jsx
@@ -1,4 +1,5 @@
-import { Container, Typography } from '@mui/material'
+import { DocumentScanner, FileDownload } from '@mui/icons-material'
+import { Avatar, Box, Button, Container, CssBaseline, Typography } from '@mui/material'
 import React from 'react'
 
 const default_blog_img = 'https://images.pexels.com/photos/3844790/pexels-photo-3844790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
@@ -7,11 +8,27 @@ const Post = () => {
     return (
         <>
             <Container maxWidth='xs'>
-                <Typography>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, unde?
-                </Typography>
-                <img src={default_blog_img}>
-                </img>
+                <Box marginBottom={1}>
+                    {/* <Avatar
+                    sx={{ position: 'fixed', zIndex: '10', bottom: '5%', right: '5%', width: '3rem', height: '3rem', backgroundColor: 'theme.palette.primary' }}>
+                    <Button size='medium' >
+                        <FileDownload fontSize='large'></FileDownload>
+                    </Button>
+                </Avatar> */}
+                    <Typography gutterBottom variant='h6' fontWeight='bold'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, unde?
+                    </Typography>
+                    <img src={default_blog_img} style={{ width: '100%' }}>
+                    </img>
+                    <Button variant='contained' fullWidth>
+                        Download PDF
+                    </Button>
+                </Box>
+                <Box>
+                    <Typography variant='body1'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error blanditiis corrupti aliquid, maxime assumenda quibusdam magni? Distinctio, maxime adipisci quaerat necessitatibus reprehenderit nam totam id placeat eveniet repudiandae harum architecto ut ipsum similique quibusdam neque tempora delectus debitis, quis, ab fugiat illum itaque! Dolor ut maiores impedit earum eum quam ipsum quia dolorem quisquam veniam vitae adipisci aliquam quidem eveniet non nostrum nisi dignissimos commodi repudiandae ipsa aliquid, recusandae quae reiciendis! Earum, sint. Consequuntur enim repellendus saepe dolore, rem eos alias! Facere laborum nihil, vero non adipisci veniam nostrum minima aspernatur consectetur officiis earum dolorum perspiciatis ducimus eos commodi aliquam.
+                    </Typography>
+                </Box>
             </Container>
         </>
     )
