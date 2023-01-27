f5ce474d52f2bec6d2956febd976345c096c478d
ayush391 <stringakaak@gmail.com>
2023-01-27 15:39:06 +0530

    updated folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { CssBaseline } from '@mui/material'
 import { useState } from 'react'
 import { HashRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './assets/components/navbar'
-import ScrollToTop from './assets/components/scrolltotop'
-import Home from './assets/pages/home'
-import Post from './assets/pages/post'
+import Navbar from './components/navbar'
+import ScrollToTop from './components/scrolltotop'
+import Home from './pages/home'
+import Post from './pages/post'
 import './App.css'
 
 function App() {
